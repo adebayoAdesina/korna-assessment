40b76c5f1661f3c70c67dad4569f60cd586ce17a
Adebayo Adesina <adebayoadesina789@gmail.com>
2025-09-26 10:24:08 +0100

    Initial korna assessment set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./App.styles";
+import Index from "./views/Index";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Index />
+    </>
+  );
+}
+
+export default App;
